f3ccb54d43d14b082ea4bf6e6323c7687a1251da
addbook <addbook@live.com>
2017-10-19 22:11:44 +0800

    Update test_3

--- practice/test_3.py
+++ practice/test_3.py
@@ -4,7 +4,7 @@ import os
 print [i for i in os.listdir("./") if i.endswith(".py")]
 
 # 打印txt的内容
-with open("test_2.txt", "r+") as f: print [i for i in f.readlines()]
+with open("test_3.txt", "r+") as f: print [i for i in f.readlines()]
 
 # 导入pprint
 import pprint
@@ -12,4 +12,8 @@ import pprint
 
 # 定义打印函数
 def print_(s):print s
-with open("test_2.txt", "r+") as f:map(lambda x: print_(x),  [i for i in f.readlines()])
+with open("test_3.txt", "r+") as f:[print_(i) for i in f.readlines()]
+
+# 将查找的 py，文件写入一个txt文件
+with open("test_3_write.txt", "w+") as f:
+    map(lambda x: f.writelines(x + "\n"), [i for i in os.listdir("./") if i.endswith(".py")])
