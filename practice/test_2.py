6fcf0fd7c324d9e55be8813ee340204f1e091525
addbook <addbook@live.com>
2017-10-19 22:26:25 +0800

    Update project

--- practice/test_2.py
+++ practice/test_2.py
@@ -5,7 +5,7 @@ f = test_import.DirInfo(fd)
 f.print_dir_pyfiles()
 f_ = test_import.DirInfo("../")
 print "type(test_import): %s  -- type(f_): %s" % (type(test_import), type(f_))
-f_.print_dir_pyfiles()
+f_.print_dir_pyfiles(); print f_.file_dir
 a = [1, "cc", ['s', 'a'], 2, 3, "sss"]
 f_.print_list(a)
 test_import.DirInfo.print_list([1, 3, 5])
