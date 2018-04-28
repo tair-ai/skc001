c20a6c36293b4192d7544ee091cb84eaae403a83
addbook <addbook@live.com>
2018-04-28 13:35:45 +0800

    IMAGE PROCESSING

--- practice/test_4_image.py
+++ practice/test_4_image.py
@@ -3,10 +3,16 @@
 
 from skimage import io
 import matplotlib.pyplot as plt
-
+import numpy as np
 face = io.imread("test_img.jpg")
 print face
 print type(face)
+print face.shape
 plt.imshow(face)
 plt.show()
-
+gray_img = 0.299 * face[:,:,0] + 0.587 * face[:,:,1] + 0.114 * face[:,:,2]
+gray_img = gray_img.astype(np.uint8)
+print gray_img.shape
+plt.imshow(gray_img)
+plt.show()
+io.imsave("test_4_new.jpg", gray_img)
